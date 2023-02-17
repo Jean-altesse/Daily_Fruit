0f255978a67f6dc471af59e505e374b3e21a98d6
Jean <Jeanaltesse@gamil.com>
2023-02-17 16:55:24 +0000

    modif du ven soir

--- Css/beggin2.py
+++ Css/beggin2.py
@@ -15,10 +15,17 @@
    #     if nbrpremier(i) :
    #         tab.append(i)
    # print(i)
-import random
 
-
-tab =['bonjour','matin' ,'activité', 'monsieur' , 'different']
-random.choice(tab)
-
+def facto(a) :
+    if a < 0 or a > 15 :
+        return -1
+    else :
+        fact = 1
+        cal = 1
+        while cal < a :
+            fact = fact * (cal+1)
+            cal = cal + 1
+        return fact
+    
+print(facto(10))
 
