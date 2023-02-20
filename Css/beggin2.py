71591f575e2fdee1a5c460744c4834506298b472
Jean <Jeanaltesse@gamil.com>
2023-02-20 16:56:21 +0000

    modif du 20 février

--- Css/beggin2.py
+++ Css/beggin2.py
@@ -16,16 +16,19 @@
    #         tab.append(i)
    # print(i)
 
-def facto(a) :
-    if a < 0 or a > 15 :
-        return -1
-    else :
-        fact = 1
-        cal = 1
-        while cal < a :
-            fact = fact * (cal+1)
-            cal = cal + 1
-        return fact
+#def facto(a) :
+   # if a < 0 or a > 15 :
+      #  return -1
+    #else :
+      #  fact = 1
+        #cal = 1
+       # while cal < a :
+         #   fact = fact * (cal+1)
+          #  cal = cal + 1
+        #return fact
     
-print(facto(10))
+#print(facto(10))
+
+class Humain : 
+    
 
